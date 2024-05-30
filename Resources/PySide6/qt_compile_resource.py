4ee5f498f0072d14ed3a16f0d9ae2b6c1e7359b5
Haomin Kong <a645162@qq.com>
2024-05-30 11:02:54 +0800

    [Preview Feature]Program Resources

---
+++ Resources/PySide6/qt_compile_resource.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+command = """
+pyside6-rcc \
+    resources.qrc \
+    -o ../../mot_toolkit/gui/resources/resources.py
+"""
+
+ret = os.system(command.strip())
+
+if ret != 0:
+    print("Qt Resource Compile Failed!")
+    exit(1)
+
+print("Qt Resource Compile Done!")
