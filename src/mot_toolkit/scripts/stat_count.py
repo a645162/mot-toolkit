95595f0aa8c6b6018aeaeb9256209641866a1f99
Haomin Kong <a645162@qq.com>
2024-10-28 13:16:05 +0800

    feat(Stats): Child dir

--- src/mot_toolkit/scripts/stat_count.py
+++ src/mot_toolkit/scripts/stat_count.py
@@ -35,57 +35,64 @@ def stat_count(dir_path: str) -> List[Tuple[str, str, str, str, str, str]]:
     if not os.path.isdir(dir_path):
         return []
 
-    print(dir_path)
-    # 视频名称	序列数	序列名称	帧数	总目标数	类别数
-    csv_ori_data_list: List[Tuple[str, str, str, str, str, str]] = []
+    # Onboard Onshore
+    class_list = os.listdir(dir_path)
+    for class_name in class_list:
+        class_dir_path = os.path.join(dir_path, class_name)
+        if not os.path.isdir(class_dir_path):
+            continue
 
-    video_list = walk_dir_get_dir_list(dir_path)
-    video_count = len(video_list)
-    print("Video Count:", video_count)
-    csv_ori_data_list.append((dir_path, str(video_count), "", "", "", ""))
+        print(class_dir_path)
+        # 视频名称	序列数	序列名称	帧数	总目标数	类别数
+        csv_ori_data_list: List[Tuple[str, str, str, str, str, str]] = []
 
-    for video_dir_path in video_list:
-        print(video_dir_path)
-        video_name = os.path.basename(video_dir_path)
-        sequence_list = walk_dir_get_dir_list(video_dir_path)
-        sequence_count = len(sequence_list)
-        print("\tSequence Count:", sequence_count)
+        video_list = walk_dir_get_dir_list(class_dir_path)
+        video_count = len(video_list)
+        print("Video Count:", video_count)
+        csv_ori_data_list.append((dir_path, str(video_count), "", "", "", ""))
 
-        for sequence_dir_path in sequence_list:
-            print("\t" + sequence_dir_path)
-            sequence_name = os.path.basename(sequence_dir_path)
+        for video_dir_path in video_list:
+            print(video_dir_path)
+            video_name = os.path.basename(video_dir_path)
+            sequence_list = walk_dir_get_dir_list(video_dir_path)
+            sequence_count = len(sequence_list)
+            print("\tSequence Count:", sequence_count)
 
-            annotation_directory = XAnyLabelingAnnotationDirectory()
-            annotation_directory.dir_path = sequence_dir_path
-            annotation_directory.walk_dir(recursive=False)
-            annotation_directory.sort_path(group_directory=True)
+            for sequence_dir_path in sequence_list:
+                print("\t" + sequence_dir_path)
+                sequence_name = os.path.basename(sequence_dir_path)
 
-            # Frame Count
-            frame_count = len(annotation_directory.file_list)
-            print("\t\tFrame:", frame_count)
+                annotation_directory = XAnyLabelingAnnotationDirectory()
+                annotation_directory.dir_path = sequence_dir_path
+                annotation_directory.walk_dir(recursive=False)
+                annotation_directory.sort_path(group_directory=True)
 
-            annotation_directory.load_json_files()
+                # Frame Count
+                frame_count = len(annotation_directory.file_list)
+                print("\t\tFrame:", frame_count)
 
-            # Class Sum(Same Tag)
-            class_sum_list = annotation_directory.update_label_list()
-            class_sum_count = len(class_sum_list)
-            print("\t\tClass Sum:", class_sum_count)
+                annotation_directory.load_json_files()
 
-            # Object Sum in each annotation files
-            object_sum_list = []
-            for annotation_file in annotation_directory.annotation_file:
-                object_sum_list.append(len(annotation_file.rect_annotation_list))
-            object_sum_count = sum(object_sum_list)
+                # Class Sum(Same Tag)
+                class_sum_list = annotation_directory.update_label_list()
+                class_sum_count = len(class_sum_list)
+                print("\t\tClass Sum:", class_sum_count)
 
-            print("\t\tObject Sum:", object_sum_count)
+                # Object Sum in each annotation files
+                object_sum_list = []
+                for annotation_file in annotation_directory.annotation_file:
+                    object_sum_list.append(len(annotation_file.rect_annotation_list))
+                object_sum_count = sum(object_sum_list)
 
-            csv_ori_data_list.append(
-                (
-                    video_name, str(sequence_count),
-                    sequence_name,
-                    str(frame_count), str(object_sum_count), str(class_sum_count)
+                print("\t\tObject Sum:", object_sum_count)
+
+                csv_ori_data_list.append(
+                    (
+                        video_name, str(sequence_count),
+                        sequence_name,
+                        str(frame_count), str(object_sum_count), str(class_sum_count)
+                    )
                 )
-            )
 
     # print(csv_ori_data_list)
     return csv_ori_data_list
@@ -115,10 +122,21 @@ def save_to_csv(
 
 
 if __name__ == "__main__":
+    base_path = r"H:\Datasets\TrackShipOnlineVideo\LabelMe\sea_video_20240313_part"
     result_list = \
-        stat_count(r"/mnt/h/Datasets/TrackShipOnlineVideo/sea_video_20240313_part2/OnboardTL")
+        stat_count(base_path + "1")
+    save_to_csv(result_list, "part1.csv")
+
+    result_list = \
+        stat_count(base_path + "2")
     save_to_csv(result_list, "part2.csv")
 
     result_list = \
-        stat_count(r"/mnt/h/Datasets/TrackShipOnlineVideo/sea_video_20240313_part4/OnshoreTL")
+        stat_count(base_path + "3")
+    save_to_csv(result_list, "part3.csv")
+
+    result_list = \
+        stat_count(base_path + "4")
     save_to_csv(result_list, "part4.csv")
+
+    print("Done")
