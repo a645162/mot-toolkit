f86636203d5e9d0ee42040d2ca913100c6fba883
Haomin Kong <a645162@qq.com>
2024-11-20 11:00:54 +0800

    fix(JSON): Try to fix json parse error

--- src/mot_toolkit/parser/json_parser.py
+++ src/mot_toolkit/parser/json_parser.py
@@ -1,15 +1,36 @@
 import json
+import os
 
 from mot_toolkit.parser.file_encoding import read_file_with_detect
+from mot_toolkit.utils.logs import get_logger
+from mot_toolkit.config.program_path import path_project
+
+logger = get_logger()
 
 
 def parse_json_to_dict(json_path: str) -> dict:
-    json_str = read_file_with_detect(json_path)
-    data = json.loads(json_str)
+    json_str = read_file_with_detect(json_path).strip()
+
+    if len(json_str) == 0:
+        logger.error(f"Empty JSON file: {json_path}")
+        return {}
+
+    data = {}
+
+    try:
+        data = json.loads(json_str)
+    except json.decoder.JSONDecodeError as e:
+        logger.error(f"JSONDecodeError: {e}")
+    except Exception as e:
+        logger.error(f"Exception: {e}")
+
     return data
 
 
 if __name__ == '__main__':
-    json_dict = parse_json_to_dict(r"H:\LCF\mot-toolkit\Test\00000000.json")
+    json_path = os.path.join(path_project, "Test", "00000000.json")
+
+    json_dict = parse_json_to_dict(json_path)
+
     print(json_dict)
     print(type(json_dict))
