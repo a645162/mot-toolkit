533cf513e08033719498e0885f455fd652a1e27f
Haomin Kong <a645162@qq.com>
2024-11-24 14:05:44 +0800

    feat(Export): Support samurai

---
+++ src/mot_toolkit/scripts/convert/samurai/appear_csv_to_samurai_txt.py
@@ -0,0 +1,33 @@
+# https://yangchris11.github.io/samurai/
+
+from typing import List
+import os
+import tqdm
+
+base_path: str = r"/mnt/h/Datasets/TrackShipOnlineVideo/LabelMe/"
+csv_list: List[str] = []
+
+for root, dirs, files in os.walk(base_path):
+    for file in files:
+        if file.endswith(".csv"):
+            csv_list.append(os.path.join(root, file))
+
+print(f"Found {len(csv_list)} csv files.")
+input("Press Enter to continue...")
+
+for csv_path in tqdm.tqdm(csv_list):
+    with open(csv_path, "r") as f:
+        lines = f.readlines()
+
+    lines = [line.strip() for line in lines]
+    if len(lines) != 1:
+        continue
+
+    frame, id, x, y, w, h = lines[0].split(",")
+    x, y, w, h = map(float, [x, y, w, h])
+
+    new_line = f"{x},{y},{w},{h}"
+
+    save_path = csv_path.replace(".csv", "_samurai.txt")
+    with open(save_path, "w") as f:
+        f.write(new_line)
---
+++ src/mot_toolkit/scripts/convert/samurai/del_samurai_txt.py
@@ -0,0 +1,17 @@
+# https://github.com/yangchris11/samurai
+
+import os
+from typing import List
+
+import tqdm
+
+base_path: str = r"/mnt/h/Datasets/TrackShipOnlineVideo/LabelMe/"
+txt_list: List[str] = []
+
+for root, dirs, files in os.walk(base_path):
+    for file in files:
+        if file.endswith("_samurai.txt"):
+            txt_list.append(os.path.join(root, file))
+
+for txt_path in tqdm.tqdm(txt_list):
+    os.remove(txt_path)
